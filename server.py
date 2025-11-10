6fb5c09f60cf19e8f33fc77ed2e72caef15d9531
John Kim <johnkim@Johns-MacBook-Pro.local>
2025-11-09 23:43:29 -0800

    final working API implementation

--- server.py
+++ server.py
@@ -1,32 +1,51 @@
-from fastapi import FastAPI, File, UploadFile
-from typing_extensions import Annotated
+from fastapi import FastAPI, UploadFile, File
 import uvicorn
-from utils import *
+from utils import create_graph_from_json
 from dijkstra import dijkstra
 
-# create FastAPI app
 app = FastAPI()
-
-# global variable for active graph
 active_graph = None
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to the Shortest Path Solver!"}
 
-
 @app.post("/upload_graph_json/")
-async def create_upload_file(file: UploadFile):
-    #TODO: implement this function
-    raise NotImplementedError("/upload_graph_json not yet implemented.")
+async def create_upload_file(file: UploadFile = File(...)):
+    global active_graph
+    filename = file.filename
 
+    if not filename.lower().endswith(".json"):
+        return {"Upload Error": "Invalid file type"}
+
+    active_graph = create_graph_from_json(file)
+    return {"Upload Success": filename}
 
 @app.get("/solve_shortest_path/start_node_id={start_node_id}&end_node_id={end_node_id}")
 async def get_shortest_path(start_node_id: str, end_node_id: str):
-    #TODO: implement this function
-    raise NotImplementedError("/solve_shortest_path not yet implemented.")
+    global active_graph
+
+    if active_graph is None:
+        return {"Solver Error": "No active graph, please upload a graph first."}
+    if start_node_id not in active_graph.nodes or end_node_id not in active_graph.nodes:
+        return {"Solver Error": "Invalid start or end node ID."}
+
+    start_node = active_graph.nodes[start_node_id]
+    dijkstra(active_graph, start_node)
+
+    end_node = active_graph.nodes[end_node_id]
+    path = []
+    cur = end_node
+    while cur is not None:
+        path.append(cur.id)
+        cur = cur.prev
+    path.reverse()
+
+    if len(path) == 1 and path[0] != start_node_id:
+        return {"shortest_path": None, "total_distance": None}
+
+    return {"shortest_path": path, "total_distance": end_node.dist}
 
 if __name__ == "__main__":
-    print("Server is running at http://localhost:8080")
+    print("Server running at http://localhost:8080")
     uvicorn.run(app, host="0.0.0.0", port=8080)
-    
